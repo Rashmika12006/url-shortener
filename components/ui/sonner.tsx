a6bdfb16a34573834c8119890b204f9a56ef1bbf
Rashmika Sharma <rashmikasharma913@gmail.com>
2026-04-23 09:44:15 +0530

    remove next-themes completely

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner, type ToasterProps } from "sonner"
 import { CircleCheckIcon, InfoIcon, TriangleAlertIcon, OctagonXIcon, Loader2Icon } from "lucide-react"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+
 
   return (
     <Sonner
